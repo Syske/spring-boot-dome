bc6dd1da7967b173764097e32a929785f57ab4b7
syske <715448004@qq.com>
2018-08-13 10:21:27 +0800

    Revert "spring boot dome"
    This reverts commit 829009ceef0e87c5b5672e1488746528f9099651.

--- staff-management-systemv1.0/src/main/java/io/github/syske/staffmanagementsystem/utils/UUIDUtil.java
+++
@@ -1,9 +0,0 @@
-package io.github.syske.staffmanagementsystem.utils;
-
-import java.util.UUID;
-
-public class UUIDUtil {
-    public static String UUID() {
-        return UUID.randomUUID().toString().replace("-", "");
-    }
-}
